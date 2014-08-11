9e7238e38840712c89729e0ff5d77715f960713f
minakshi-webonise <minakshi.najardhane@weboniselab.com>
2014-08-11 16:14:35 +0530

    initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '52727dae177393dc711a3c247c316c66bf78ac7545a782264ae52749077fa9be5793041f389c08c952e49016825a01e42e4d3738a57d9b361061bd2bd5ef1aea'
